644a07e14904ca7768847dd69170336e2eb0eec1
Terry Torres <terrytorres@gmail.com>
2016-12-16 09:52:52 -0500

    Swift 3 conversion complete, more or less

--- horrible house/AppDelegate.swift
+++ horrible house/AppDelegate.swift
@@ -94,7 +94,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
         let modelURL = Bundle.main.url(forResource: "horrible_house", withExtension: "momd")!
         return NSManagedObjectModel(contentsOf: modelURL)!
-    }
+    }()
 
     lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
         // The persistent store coordinator for the application. This implementation creates and returns a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
